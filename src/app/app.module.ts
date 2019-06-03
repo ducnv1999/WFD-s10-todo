2065dd56dfea9ffc0a9be6d140710db9af8ca78b
ducnv1999 <ducgau99@gmail.com>
2019-06-03 09:25:18 +0700

    finish add todo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,32 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-
+import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BlogComponent } from './blog/blog.component';
-import { BlogEditComponent } from './blog-edit/blog-edit.component';
 import { BlogDetailComponent } from './blog-detail/blog-detail.component';
-import {ReactiveFormsModule} from '@angular/forms';
-import {HttpClientModule} from '@angular/common/http';
+import { BlogComponent } from './blog/blog.component';
+import { TodoComponent } from './todo/todo.component';
+import { TokenInterceptor } from './token.interceptor';
+import { BlogEditComponent } from './blog-edit/blog-edit.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    TodoComponent,
     BlogComponent,
-    BlogEditComponent,
-    BlogDetailComponent
+    BlogDetailComponent,
+    BlogEditComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     ReactiveFormsModule,
-    HttpClientModule
+    HttpClientModule,
+    AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: TokenInterceptor, multi: true },
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
